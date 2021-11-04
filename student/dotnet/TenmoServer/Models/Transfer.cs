d9d493ab6a070e04b8a2ba4f86ceed0d14932bbf
Kiran Meyers <kiranmeyers@gmail.com>
2021-11-04 10:21:45 -0400

    Models validated

--- student/dotnet/TenmoServer/Models/Transfer.cs
+++ student/dotnet/TenmoServer/Models/Transfer.cs
@@ -1,12 +1,29 @@
-﻿namespace TenmoServer.Models
+﻿
+using System.ComponentModel.DataAnnotations;
+
+
+namespace TenmoServer.Models
 {
     public class Transfer
     {
+
+        [Range(3001, double.PositiveInfinity, ErrorMessage = "Transfer id must be OVER 3000")]
         public int TransferId { get; set; }
+        
+        [Required(ErrorMessage = "Transfer Type Required.")]
+        [Range(1, 2,ErrorMessage = "Transfer Type Id must be 1 or 2.")]
         public int TransferTypeId { get; set; }
+        
+        [Range(1, 3, ErrorMessage = "Transfer Status Id must be 1, 2 or 3.")]
         public int TransferStatusId { get; set; }
+        
+        [Required(ErrorMessage = "Sender account Id Required.")]
         public int AccountFrom { get; set; }
+        
+        [Required(ErrorMessage = "Receiver account Id Required.")]
         public int AccountTo { get; set; }
+
+        [Range(.01, double.PositiveInfinity,ErrorMessage ="Transfer amount cannot be 0 or negative.")]
         public decimal Amount { get; set; }
 
         public Transfer(int transferId, int transferTypeId, int transferStatusId, int accountFrom, int accountTo, decimal amount)
--- student/dotnet/TenmoServer/Models/User.cs
+++ student/dotnet/TenmoServer/Models/User.cs
@@ -1,11 +1,21 @@
-﻿namespace TenmoServer.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+
+namespace TenmoServer.Models
 {
     public class User
     {
+        [Required(ErrorMessage = "User id can not be blank.")]
+        [Range(1,double.PositiveInfinity)]
         public int UserId { get; set; }
+        [Required]
+        [RegularExpression(@"^(\w+)$",ErrorMessage = "Username may only have letters and numbers.")]
         public string Username { get; set; }
         public string PasswordHash { get; set; }
         public string Salt { get; set; }
+        [RegularExpression("^(.+)(@)(.+)$",ErrorMessage = "Invalid email format.")]
+
         public string Email { get; set; }
     }
 
